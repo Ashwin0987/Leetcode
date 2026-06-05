ae1f4a06b66e057366dea93abfc9d5cd7bd5d22f
Ashwin0987 <bsaashwin00@gmail.com>
2026-06-05 21:46:50 +0530

    Time: 1 ms (81.69%), Space: 42.9 MB (5.48%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -4,7 +4,7 @@ class Solution {
         while(true){
             int sum=0;
             while(n>0){
-                sum+=Math.pow(n%10,2);
+                sum+=(n%10)*(n%10);
                 n=n/10;
             }
             if(sum==1) return true;
