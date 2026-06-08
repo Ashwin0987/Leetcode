de51c228d7a604ec47777a4e0cefe558f15dee29
Ashwin B <bsaashwin00@gmail.com>
2026-06-08 22:04:12 +0530

    Time: 4 ms (99.71%), Space: 47.7 MB (65.79%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -9,12 +9,14 @@ class Solution {
     public boolean validPalindrome(String s){
         int l=0,r=s.length()-1;
         while(l<r){
-            if(s.charAt(l)==s.charAt(r)){
+            if(s.charAt(l)!=s.charAt(r)){
+                return (pal(s,l+1,r)||pal(s,l,r-1));
+            }
+            else{
                 l++;
                 r--;
             }
-            else
-            return (pal(s,l+1,r)||pal(s,l,r-1));
+            
         }
         return true;
     }
