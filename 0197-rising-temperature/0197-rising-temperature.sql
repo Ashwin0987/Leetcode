4e848821ac7db36d6ad4a66e369bdebb320ba313
Ashwin B <bsaashwin00@gmail.com>
2026-07-01 21:52:31 +0530

    Time: 619 ms (9.95%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 select t1.id from weather t1 
-join Weather t2 
-on datediff(t1.recordDate,t2.recordDate )=1 
+join Weather t2 on datediff(t1.recordDate,t2.recordDate )=1 
 where t1.temperature>t2.temperature 
